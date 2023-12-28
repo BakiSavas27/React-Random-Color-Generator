c262b4671b8f74c7aaeaeceda9daab57559ce5f2
BakiSavas27 <savasbaki25@gmail.com>
2023-12-28 15:57:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [hex, setHex] = useState("#ffffff")
+
+  function randomHex(){
+    const randomhex ="#" + Math.floor(Math.random() * 16777721).toString(16);
+    setHex(randomhex);
+  }
+
+  return (
+    <div className='App' style={{backgroundColor:`${hex}`}}>
+      <h1>{hex}</h1>
+      <button onClick={randomHex}>Click For New Background</button>
+      <button onClick={()=>navigator.clipboard.writeText(hex)}>Copy The Hex Value</button>
+     
+    </div>
+  )
+}
+
+export default App
